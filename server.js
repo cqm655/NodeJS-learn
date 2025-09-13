cdaf1a866a9b4df59fd95a71a5be126f02610218
cqm655 <b.iura1992@gmail.com>
2025-09-13 22:43:40 +0300

    write in file from input

--- server.js
+++ server.js
@@ -5,8 +5,23 @@ const server = http.createServer((req, res) => {
     const url = req.url
     const method = req.method
     if (url === '/' && method === 'GET') {
-        fs.writeFileSync('test.tx', 'dummy')
-        return res.end()
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>')
+        res.write('</body>')
+        res.write('</html>')
+    }
+    if (method === 'POST' && url === '/message') {
+
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString() //buff and concat to add all chunks
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
     }
 })
 
