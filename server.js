66f34a0174888207e4cfc0fd43980784351519e1
cqm655 <b.iura1992@gmail.com>
2025-09-14 10:04:50 +0300

    aded routing

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-    const method = req.method
-    if (url === '/' && method === 'GET') {
-        res.write('<html>')
-        res.write('<body>')
-        res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>')
-        res.write('</body>')
-        res.write('</html>')
-    }
-    if (method === 'POST' && url === '/message') {
+const routes = require("./routes");
 
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString() //buff and concat to add all chunks
-            const message = parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt', message)
-        })
-    }
-})
+const server = http.createServer(routes.handler)
 
 server.listen(3003)
